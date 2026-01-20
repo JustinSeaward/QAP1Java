9ffdf69c612ecb99e6568534d27106da8ba20b9f
Justin Seaward <jut_seaward@yahoo.com>
2026-01-19 23:54:25 -0330

    added getters for ID, name & balance. Addec  methods for credit, debt and transferTo

--- src/Problem1/Account.java
+++ src/Problem1/Account.java
@@ -3,7 +3,8 @@ package Problem1;
 public class Account {
     private String id;
     private String name;
-    private int balance;
+    private int balance = 0;
+    private int amount;
 
     public Account(String id, String name){
         this.id = id;
@@ -16,6 +17,36 @@ public class Account {
         this.balance = balance;
     }
 
+    public String getID(){
+        return id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getBalance(int i){
+        return balance;
+    }
+
+    public int credit(int amount){
+        balance = amount + balance;
+       return balance;
+    }
+
+    public int debt(int amount){
+        if(amount <= balance ){
+            return amount - balance;
+        } else {
+            System.out.println("Amount exceeded balance");
+            return balance;
+        }
+    }
+
+    public int transferTo(Account another, int amount){
+        if(amount <= balance){
+
+        }
+    }
 
 }
