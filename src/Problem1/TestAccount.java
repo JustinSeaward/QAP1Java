f3efa901816268ee3d9aed188c2b3c7a92a4096c
Justin Seaward <jut_seaward@yahoo.com>
2026-01-22 12:12:09 -0330

    accounts added, balances display and transfer working

--- src/Problem1/TestAccount.java
+++ src/Problem1/TestAccount.java
@@ -1,4 +1,16 @@
 package Problem1;
 
 public class TestAccount {
+    public static void main(String[] args ){
+        Account acc1 = new Account("1", "Rusty Shackford", 5000);
+        Account acc2 = new Account("2","John Doe",4000);
+        System.out.println("");
+        System.out.println("Account 1: " + acc1.getBalance());
+        System.out.println("Account 2: " + acc2.getBalance());
+        acc1.transferTo(acc2,6000);
+        System.out.println("");
+        System.out.println("Account 1: " + acc1.getBalance());
+        System.out.println("Account 2: " + acc2.getBalance());
+    }
 }
+
