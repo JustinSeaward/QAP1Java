578995820a1dbb6bdc1ee5a6f70f1e79a185e01a
Justin Seaward <jut_seaward@yahoo.com>
2026-01-23 00:21:18 -0330

    added instance variables, Date constructor, Getters and setters for day, month and year and toString method.

--- src/Problem2/Date.java
+++ src/Problem2/Date.java
@@ -1,4 +1,49 @@
 package Problem2;
 
 public class Date {
+    //Instance variables
+    private int day = 00;
+    private int month = 00;
+    private int year;
+
+    // Constructors
+    public Date(int day, int month, int year){
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public int getDay(){
+        return this.day;
+    }
+
+    public int getMonth(){
+        return this.month;
+    }
+
+    public int getYear(){
+        return this.year;
+    }
+
+    public void setDay(int day){
+        this.day = day;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setDate(int day, int month, int year){
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public String toString() {
+        return String.format("%02d/%02d/%d", this.day, this.month, this.year);
+    }
 }
