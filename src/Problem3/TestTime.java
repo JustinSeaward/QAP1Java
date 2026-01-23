21de3744e68486b8db94f7b146554f7fd59dc17d
Justin Seaward <jut_seaward@yahoo.com>
2026-01-23 00:29:34 -0330

    Created two Time objects and set their time. Printed the current time for both objects. Added a second to t1. Decreased a second on t2. Printed both new times for t1 and t2.

--- src/Problem3/TestTime.java
+++ src/Problem3/TestTime.java
@@ -1,4 +1,15 @@
 package Problem3;
 
 public class TestTime {
+    public static void main(String[] args) {
+        Time t1 = new Time(21,10,15);
+        Time t2 = new Time(10,20,25);
+        System.out.printf("Time 1: " + t1 +"\n");
+        System.out.println("Time 2: " + t2);
+        System.out.println("\n");
+        t1.nextSecond();
+        t2.previousSecond();
+        System.out.println("Time 1: " + t1.toString());
+        System.out.println("Time 2: " + t2.toString());
+    }
 }
