47448540b6f711465ff5b5c9637f2a8ae3459b0c
Justin Seaward <jut_seaward@yahoo.com>
2026-01-25 13:40:37 -0330

    blank line added for readability

--- src/Problem2/TestDate.java
+++ src/Problem2/TestDate.java
@@ -3,6 +3,7 @@ package Problem2;
 public class TestDate {
     static void main(String[] args) {
         Date d1 = new Date(10,10,1989);
+        System.out.println("\n");
         System.out.println(d1.toString());
     }
 }
